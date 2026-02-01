af3056ac855abc81c30cef1b5f352e5f10e47c91
BaselAsh <pasel.ashraf.eg@gmail.com>
2026-02-01 22:36:51 +0200

    Second assignment complete.

---
+++ SecondAssignment/Program.cs
@@ -0,0 +1,15 @@
+﻿long n1;
+long n2;
+long n3;
+
+Console.WriteLine("--This app calculates the average of 3 numbers--");
+
+Console.Write("Enter The First Number: ");
+n1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter The Second Number: ");
+n2 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter The Third Number: ");
+n3 = int.Parse(Console.ReadLine()!);
+
+double average = (n1 + n2 + n3) / 3;
+Console.WriteLine("The average of the 3 numbers is " + average);
