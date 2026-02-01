7b929558f3d050f9e67c93470284611b9fcf00d7
BaselAsh <pasel.ashraf.eg@gmail.com>
2026-02-01 22:22:51 +0200

    feat: First assignment complete.

---
+++ FirstAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string name;
+int age;
+string city;
+
+
+Console.Write("Enter Your Name: ");
+name = Console.ReadLine()!;
+Console.Write("Enter Your Age: ");
+age = int.Parse(Console.ReadLine()!);
+Console.Write("Enter Your City: ");
+city = Console.ReadLine()!;
+
+Console.WriteLine("My name is " + name + ".");
+Console.WriteLine("I'm " + age + " Years Old.");
+Console.WriteLine("I live in " + city + ".");
+
+
