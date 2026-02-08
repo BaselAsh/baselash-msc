f6db9d2398fa0e756de8a85761d5be5b44ef079e
BaselAsh <pasel.ashraf.eg@gmail.com>
2026-02-08 22:57:05 +0200

    Grading system assignment complete.

---
+++ GradingSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿float gpa;
+
+Console.Write("Enter Your GPA: ");
+gpa = float.Parse(Console.ReadLine());
+
+if (gpa < 1 && gpa > 0)
+{
+    Console.WriteLine("Failed");
+}
+else if (gpa >= 1 && gpa <= 2.5)
+{
+    Console.WriteLine("Good");
+}
+else if (gpa > 2.5 && gpa <= 3.5)
+{
+    Console.WriteLine("Very Good");
+}
+else if (gpa > 3.5 && gpa <= 4)
+{
+    Console.WriteLine("Excellent");
+}
+else
+{
+    Console.WriteLine("Something Went Wrong");
+}
