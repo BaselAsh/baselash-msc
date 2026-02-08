86f422ae295b44ce5d68bcea73fbf985c0f0d7d0
BaselAsh <pasel.ashraf.eg@gmail.com>
2026-02-08 23:11:45 +0200

    Menu assignment complete.

---
+++ Menu/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("1 --> Adding 2 Numbers");
+Console.WriteLine("2 --> Subtract 2 Numbers");
+Console.WriteLine("3 --> Multiply 2 Numbers");
+Console.WriteLine("4 --> Divide 2 Numbers");
+Console.Write("Please Choose an Operation: ");
+int selection = int.Parse(Console.ReadLine());
+double FirstNumber;
+double SecondNumber;
+if (selection >= 1 && selection <= 4)
+{
+    Console.Write("Enter The First Number: ");
+    FirstNumber = double.Parse(Console.ReadLine());
+
+    Console.Write("Enter The Second Number: ");
+    SecondNumber = double.Parse(Console.ReadLine());
+}
+else
+{
+    Console.WriteLine("Please choose an operation between 1 and 4.");
+    return;
+}
+
+switch (selection)
+{
+    case 1:
+        Console.WriteLine($"{FirstNumber} + {SecondNumber} = {FirstNumber + SecondNumber}");
+        break;
+    case 2:
+        Console.WriteLine($"{FirstNumber} - {SecondNumber} = {FirstNumber - SecondNumber}");
+        break;
+    case 3:
+        Console.WriteLine($"{FirstNumber} * {SecondNumber} = {FirstNumber * SecondNumber}");
+        break;
+    case 4:
+        Console.WriteLine($"{FirstNumber} / {SecondNumber} = {FirstNumber / SecondNumber}");
+        break;
+    default:
+        Console.WriteLine("Please choose an operation between 1 and 4.");
+        break;
+}
+
