2578331ddfb25c74724a7a68c9ad4d2332893542
BaselAsh <pasel.ashraf.eg@gmail.com>
2026-02-08 23:15:51 +0200

    Even or odd detector assignment complete.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter A Number: ");
+int user_input = int.Parse(Console.ReadLine());
+
+if (user_input % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+
+    Console.WriteLine("Odd");
+}
